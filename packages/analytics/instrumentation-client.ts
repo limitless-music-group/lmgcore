d7e335bb5d90265b975e11bee7929dfbb99ec7c1
Erick Winston <erickwinston391@gmail.com>
2025-12-10 12:22:24 -0500

    Coding more stuff

---
+++ packages/analytics/instrumentation-client.ts
@@ -0,0 +1,9 @@
+import posthog from "posthog-js";
+import { keys } from "./keys";
+
+export const initializeAnalyticsClient = () => {
+    posthog.init(keys().NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: keys().NEXT_PUBLIC_POSTHOG_HOST,
+        defaults: '2025-11-30'
+    })
+}
